2865e47eb6c9b20430d0139d0fb5822d8925bd9c
11240 <1124061609@qq.com>
2023-08-02 22:21:02 +0800

    use const

--- constDemo.cpp
+++ constDemo.cpp
@@ -1,14 +1,21 @@
 #include<iostream>
 using namespace std;
 
-#define LENGTH 10
-#define WIDTH 5
-#define NEWLINE '\n'
+// #define LENGTH 10
+// #define WIDTH 5
+// #define NEWLINE '\n'
+
+const int LENGTH =10;
+const int WIDTH = 6;
+const char NEWLINE = '\n';
+
 int main()
 {
     int area;
     area=LENGTH*WIDTH;
     cout << area << endl;
     cout << NEWLINE << endl;
+
+
     return 0;
 }
